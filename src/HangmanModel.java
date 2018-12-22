566c626dce9f53273446c9ded664ba0d69b6b59e
BurakMSimsek <burak.simsek.6722@ozu.edu.tr>
2018-12-23 01:44:40 +0300

    Updated Model

--- src/HangmanModel.java
+++ src/HangmanModel.java
@@ -1,6 +1,8 @@
 
 public class HangmanModel {
 	private String secretWord;
+	private int wordLength;
+	private String difficultyType;
 	
 	public boolean isCorrectLetter(String name,char ch) {
 		for (int i=0;i<name.length();i++) {
@@ -18,5 +20,19 @@ public class HangmanModel {
 	public void setSecretWord(String secretWord) {
 		this.secretWord = secretWord;
 	}
+
+	public int getWordLength() {
+		return wordLength;
+	}
+
+	public String getDifficultyType() {
+		return difficultyType;
+	}
+
+	public void setDifficultyType(String difficultyType) {
+		this.difficultyType = difficultyType;
+	}
+
+
 	
 }
