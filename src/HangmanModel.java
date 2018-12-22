b1e23e7d0f73e7314a8e7188a791ae5807d1932c
BurakMSimsek <burak.simsek.6722@ozu.edu.tr>
2018-12-23 01:36:37 +0300

    Updated Model

--- src/HangmanModel.java
+++ src/HangmanModel.java
@@ -2,4 +2,12 @@
 public class HangmanModel {
 	private String secretWord;
 	
+	public boolean isCorrectLetter(String name,char ch) {
+		for (int i=0;i<name.length();i++) {
+			if(ch==name.charAt(i)) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
