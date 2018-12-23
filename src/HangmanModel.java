3c8db76ff85cbe80c4c4de6f2e375106925bafc7
W0ltage <saygnpoyraz@gmail.com>
2018-12-24 00:42:50 +0300

    json file problems

--- src/HangmanModel.java
+++ src/HangmanModel.java
@@ -1,3 +1,13 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Random;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class HangmanModel {
 	private String secretWord;
@@ -14,6 +24,7 @@ public class HangmanModel {
 		}
 		return false;
 	}
+
 	public int returnIndexOfLetter(char ch) {
 		for (int i = 0; i < secretWord.length(); i++) {
 			if (ch == secretWord.charAt(i)) {
@@ -41,12 +52,49 @@ public class HangmanModel {
 		appearLetters = new boolean[secretWord.length()];
 	}
 
+	@SuppressWarnings("deprecation")
 	public String getSecretWordFromJSON() {
-		return "poyraz";
+		try {
+			Random random = new Random();
+			int randomNumber = random.nextInt(99) + 1;
+			System.out.println("girdi");
+			JSONParser parser = new JSONParser();
+			Object object = parser.parse(new FileReader("JSON.json"));
+			JSONObject jsonObject = (JSONObject) object;
+			JSONArray easy = (JSONArray) jsonObject.get("easy");
+			JSONArray medium = (JSONArray) jsonObject.get("medium");
+			JSONArray hard = (JSONArray) jsonObject.get("hard");
+			Iterator<String> iterator;
+			System.out.println(difficultyType);
+			if (difficultyType.equals("Easy")) {
+				System.out.println("EZgirdi");
+				iterator = easy.iterator();
+			} else if (difficultyType.equals("Medium")) {
+				iterator = medium.iterator();
+				System.out.println("Medgirdi");
+			} else {
+				iterator = hard.iterator();
+				System.out.println("Hardgirdi");
+
+			}
+
+			while (iterator.hasNext()) {
+				System.out.println(iterator.next());
+				randomNumber--;
+				if (randomNumber == 0) {
+					return iterator.next();
+				}
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		return "error";
 	}
-	
+
 	public void guessWord(String guess) {
-		
+
 	}
 
 }
