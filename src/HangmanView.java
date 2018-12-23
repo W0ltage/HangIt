f2f1fb65672d71adcb1f7d7c3fbb777ecd86cf92
BurakMSimsek <burak.simsek.6722@ozu.edu.tr>
2018-12-23 21:18:19 +0300

    View updated

--- src/HangmanView.java
+++ src/HangmanView.java
@@ -93,22 +93,80 @@ public class HangmanView {
 		answerPanel.setLayout(new BoxLayout(answerPanel, BoxLayout.LINE_AXIS));
 		answerField = new JTextField();
 		guess = new JButton("Guess the letter");
+		JPanel buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(new GridLayout(2, 13));
+        JButton b1 = new JButton("A");
+        JButton b2 = new JButton("B");
+        JButton b3 = new JButton("C");
+        JButton b4 = new JButton("D");
+        JButton b5 = new JButton("E");
+        JButton b6 = new JButton("F");
+        JButton b7 = new JButton("G");
+        JButton b8 = new JButton("H");
+        JButton b9 = new JButton("I");
+        JButton b10 = new JButton("J");
+        JButton b11 = new JButton("K");
+        JButton b12 = new JButton("L");
+        JButton b13 = new JButton("M");
+        JButton b14 = new JButton("N");
+        JButton b15 = new JButton("O");
+        JButton b16 = new JButton("P");
+        JButton b17 = new JButton("Q");
+        JButton b18 = new JButton("R");
+        JButton b19 = new JButton("S");
+        JButton b20 = new JButton("T");
+        JButton b21 = new JButton("U");
+        JButton b22 = new JButton("V");
+        JButton b23 = new JButton("W");
+        JButton b24 = new JButton("X");
+        JButton b25 = new JButton("Y");
+        JButton b26 = new JButton("Z");
+        buttonsPanel.add(b1);
+        buttonsPanel.add(b2);
+        buttonsPanel.add(b3);
+        buttonsPanel.add(b4);
+        buttonsPanel.add(b5);
+        buttonsPanel.add(b6);
+        buttonsPanel.add(b7);
+        buttonsPanel.add(b8);
+        buttonsPanel.add(b9);
+        buttonsPanel.add(b10);
+        buttonsPanel.add(b11);
+        buttonsPanel.add(b12);
+        buttonsPanel.add(b13);
+        buttonsPanel.add(b14);
+        buttonsPanel.add(b15);
+        buttonsPanel.add(b16);
+        buttonsPanel.add(b17);
+        buttonsPanel.add(b18);
+        buttonsPanel.add(b19);
+        buttonsPanel.add(b20);
+        buttonsPanel.add(b21);
+        buttonsPanel.add(b22);
+        buttonsPanel.add(b23);
+        buttonsPanel.add(b24);
+        buttonsPanel.add(b25);
+        buttonsPanel.add(b26);
+
 
 		JPanel competitorPanel = new JPanel(new BorderLayout());
 		competitorPanel.add(BorderLayout.CENTER, answerField);
 		competitorPanel.add(BorderLayout.EAST, guess);
+		competitorPanel.add(BorderLayout.SOUTH,buttonsPanel);
 		answerPanel.add(competitorPanel);
+		
+		
 
 		game.add(answerPanel);
 
-		JPanel buttonsPanel = new JPanel();
+		/*JPanel buttonsPanel = new JPanel();
 		char ch = (char) 65;
 		for (int i = 0; i < 26; i++) {
 			buttonsPanel.add(new JButton(ch + ""));
 			ch++;
 		}
-		//game.add(buttonsPanel);
-
+		game.add(buttonsPanel);
+*/
 		game.setVisible(true);
 	}
 
