5778200019c489cc691b4f9c8c5f6f81a6beb3d8
W0ltage <saygnpoyraz@gmail.com>
2018-12-24 00:53:30 +0300

    changes apply

--- src/HangmanModel.java
+++ src/HangmanModel.java
@@ -48,48 +48,12 @@ public class HangmanModel {
 
 	public void setDifficultyType(String difficultyType) {
 		this.difficultyType = difficultyType;
-		secretWord = getSecretWordFromJSON();
+		secretWord = getSecretWordFromFile();
 		appearLetters = new boolean[secretWord.length()];
 	}
 
-	@SuppressWarnings("deprecation")
-	public String getSecretWordFromJSON() {
-		try {
-			Random random = new Random();
-			int randomNumber = random.nextInt(99) + 1;
-			System.out.println("girdi");
-			JSONParser parser = new JSONParser();
-			Object object = parser.parse(new FileReader("JSON.json"));
-			JSONObject jsonObject = (JSONObject) object;
-			JSONArray easy = (JSONArray) jsonObject.get("easy");
-			JSONArray medium = (JSONArray) jsonObject.get("medium");
-			JSONArray hard = (JSONArray) jsonObject.get("hard");
-			Iterator<String> iterator;
-			System.out.println(difficultyType);
-			if (difficultyType.equals("Easy")) {
-				System.out.println("EZgirdi");
-				iterator = easy.iterator();
-			} else if (difficultyType.equals("Medium")) {
-				iterator = medium.iterator();
-				System.out.println("Medgirdi");
-			} else {
-				iterator = hard.iterator();
-				System.out.println("Hardgirdi");
-
-			}
-
-			while (iterator.hasNext()) {
-				System.out.println(iterator.next());
-				randomNumber--;
-				if (randomNumber == 0) {
-					return iterator.next();
-				}
-			}
-
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-
+	public String getSecretWordFromFile() {
+		// get text from text file
 		return "error";
 	}
 
